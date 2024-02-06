94af5a584ddb59d0dd8a42a8ac1ef3b1a4838ce3
IIHT Developers <93914497+IIHTDevelopers@users.noreply.github.com>
2024-02-06 14:57:14 +0530

    Update Skills.java

--- skills-service-template/src/main/java/com/iiht/training/skills/entity/Skills.java
+++ skills-service-template/src/main/java/com/iiht/training/skills/entity/Skills.java
@@ -2,13 +2,15 @@ package com.iiht.training.skills.entity;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
-
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 
 
 @Entity
 public class Skills {
 
 	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private Integer employeeId;
 	private String name;
