15b28553c8c71378d4aefe63a2dd93f2c2a1b79c
IIHT Developers <93914497+IIHTDevelopers@users.noreply.github.com>
2024-02-06 14:50:30 +0530

    Update Employee.java

--- employee-service-template/src/main/java/com/iiht/training/employee/entity/Employee.java
+++ employee-service-template/src/main/java/com/iiht/training/employee/entity/Employee.java
@@ -2,13 +2,15 @@ package com.iiht.training.employee.entity;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
-
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 
 
 @Entity
 public class Employee {
 
 	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String username;
 	private String password;
