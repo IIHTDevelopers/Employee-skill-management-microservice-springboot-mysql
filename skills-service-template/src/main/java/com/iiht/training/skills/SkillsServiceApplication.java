ffa3b8560bdd2831fa90ff304d954a9027cb083a
IIHT Developers <93914497+IIHTDevelopers@users.noreply.github.com>
2024-02-07 16:12:25 +0530

    Update SkillsServiceApplication.java

--- skills-service-template/src/main/java/com/iiht/training/skills/SkillsServiceApplication.java
+++ skills-service-template/src/main/java/com/iiht/training/skills/SkillsServiceApplication.java
@@ -2,10 +2,9 @@ package com.iiht.training.skills;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.openfeign.EnableFeignClients;
+
 
 @SpringBootApplication
-@EnableFeignClients
 public class SkillsServiceApplication {
 
 	public static void main(String[] args) {
