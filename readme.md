d0313cf58c8c069ee2b230cf72646025c81ce7dc
IIHT Developers <93914497+IIHTDevelopers@users.noreply.github.com>
2024-04-03 20:13:10 +0530

    Add files via upload

---
+++ readme.md
@@ -0,0 +1,7 @@
+### Identity Service
+
+- This Module is used to validate user identity.
+- Register user in system.
+- Generate User Token using registered user credentials
+- Validate User Token
+---
--- skills-service-template/src/main/java/com/iiht/training/skills/proxy/CertificatesServiceProxy.java
+++ skills-service-template/src/main/java/com/iiht/training/skills/proxy/CertificatesServiceProxy.java
@@ -3,13 +3,15 @@ package com.iiht.training.skills.proxy;
 import java.util.List;
 
 
+import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 import com.iiht.training.skills.dto.CertificatesDto;
 
+@FeignClient(name = "certificates-service")
 public interface CertificatesServiceProxy {
-
+	@GetMapping("/api/certificates")
 	// Add a correct configuration for feign client
 	public List<CertificatesDto> getCertificatesBySkillName(@PathVariable String skillName); 
 }
